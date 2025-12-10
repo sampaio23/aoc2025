d82358f66b409e1ceb80f7458a33a2307ba9c8bc
Luiz Sampaio <sampaio.ime@gmail.com>
2025-12-10 12:24:31 +0000

    [04] Part 2 done

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -40,7 +40,52 @@ fn part1(s: &String) -> u32 {
 }
 
 fn part2(s: &String) -> u32 {
-    todo!();
+    let width = s.lines().nth(0).unwrap().len();
+    let height = s.lines().count();
+
+    let mut vec = vec![0; (width + 2) * (height + 2)];
+
+    for i in 1..=height {
+        for j in 1..=width {
+            if s.lines().nth(i-1).unwrap().chars().nth(j-1).unwrap() == '@' {
+                vec[i*(width+2) + j] = 1;
+            }
+        }
+    }
+
+    let mut total = 0;
+    let mut iteration_total = 0;
+
+    loop {
+        for i in 1..=height {
+            for j in 1..=width {
+                if vec[i*(width+2) + j] == 0 {
+                    continue;
+                }
+
+                let mut sum = 0;
+                for k in 0..=2 {
+                    for l in 0..=2 {
+                        if k != 1 || l != 1 {
+                            sum += vec[(i+k-1)*(width+2) + (j+l-1)];
+                        }
+                    }
+                }
+                if sum < 4 {
+                    iteration_total += 1;
+                    vec[i*(width+2) + j] = 0;
+                }
+            }
+        }
+
+        if iteration_total == 0 {
+            break;
+        }
+        total += iteration_total;
+        iteration_total = 0;
+    }
+
+    return total;
 }
 
 fn main() {
